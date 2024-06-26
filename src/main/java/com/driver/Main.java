ed73f0d9fee9ab9d22cb2724c1022e9ddaf42efb
kiran-0103vs <kiranvenkatesan08@gmail.com>
2024-06-26 21:32:21 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-
+        A objA = new A();
+        System.out.println(objA.meth());
         B objB = new B();
         System.out.println(objB.meth());
 
